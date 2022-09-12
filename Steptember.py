17b4217350fb87f963e9abec5044a6a907e86ef7
ZanePearton <z.pearton@accenture.com>
2022-09-12 17:55:10 +1000

    push

---
+++ Steptember.py
@@ -0,0 +1,35 @@
+#import text
+from cgitb import text
+#import requests
+import requests;
+#import sleep
+from time import sleep
+#min Count 
+count = 0; 
+#max count 
+max_count = 50; 
+#while true do the following 
+while True:
+    #inscrements of 50
+    count += 50
+    #count concat with str
+    URL = "https://www.steptember.org.au/funraisin/viewlump/4688?offset=";
+    #sleep 1 second 
+    sleep (1);
+    #concat url with path
+    con_url = (URL + str (count)); 
+    #print path
+    print (con_url)
+    #send request
+    r = requests.get(con_url);
+    # print (r.text)
+    output = str (r.text);
+    # str output = (r.text);
+    with open('Steptember.txt', 'w') as f:
+        #write 
+        f.write(output)
+    #break
+    if count >= max_count:
+        #exceeds max count break
+        break
+    print ("complete")

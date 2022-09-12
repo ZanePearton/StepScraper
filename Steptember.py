1407ba3fbb99fccd8a71968812c6b36f05db586a
ZanePearton <z.pearton@accenture.com>
2022-09-12 20:11:09 +1000

    html

--- Steptember.py
+++ Steptember.py
@@ -7,13 +7,13 @@ from time import sleep
 #min Count 
 count = 0; 
 #max count 
-max_count = 50; 
+max_count = 150; 
 #while true do the following 
 while True:
     #inscrements of 50
-    count += 50
     #count concat with str
     URL = "https://www.steptember.org.au/funraisin/viewlump/4688?offset=";
+    
     #sleep 1 second 
     sleep (1);
     #concat url with path
@@ -28,7 +28,7 @@ while True:
     with open('Steptember.html', 'w') as f:
         #write 
         f.write(output)
-
+    count += 50
     #break
     if count >= max_count:
         #exceeds max count break
